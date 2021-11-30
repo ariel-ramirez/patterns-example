1405688adf48d590f8b9554554ad8ced775b54bb
federiconosmor <federico.nosmor@mercadolibre.com>
2021-11-30 15:33:51 -0300

    patters example: - factory - adapter - bridge - decorator - strategy

---
+++ src/factory/dialog_factory/HtmlDialog.java
@@ -0,0 +1,15 @@
+package factory.dialog_factory;
+
+import factory.buttons.Button;
+import factory.buttons.HtmlButton;
+
+/**
+ * HTML Dialog will produce HTML buttons.
+ */
+public class HtmlDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new HtmlButton();
+    }
+}
---
+++ src/factory/dialog_factory/WindowsDialog.java
@@ -0,0 +1,15 @@
+package factory.dialog_factory;
+
+import factory.buttons.Button;
+import factory.buttons.WindowsButton;
+
+/**
+ * Windows Dialog will produce Windows buttons.
+ */
+public class WindowsDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
